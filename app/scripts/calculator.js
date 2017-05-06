db5b4591493cda17745c7d3c8b1929ad15c69397
mhartz <mshartz5@gmail.com>
2017-05-06 01:43:27 -0400

    testing some git command stuff - pray for no break

--- app/scripts/calculator.js
+++ app/scripts/calculator.js
@@ -3,18 +3,37 @@ var Calculator = {
     document.querySelector('#submit-add').addEventListener('click', function() {
       Calculator.add();
     });
+    
+    document.querySelector('#submit-subtract').addEventListener('click', function() {
+      Calculator.subtract();
+    });
   },
-  
+
   add: function() {
     var firstNumber,
+    secondNumber,
+    result,
+    returns;
+
+    firstNumber = document.querySelector('#add-first-number').value;
+    secondNumber = document.querySelector('#add-second-number').value;
+    result = document.querySelector('#add-result');
+
+    returns = +firstNumber + +secondNumber;
+    result.innerHTML = "= " + returns;
+  },
+
+  subtract: function() {
+    var firstNumber,
         secondNumber,
-        result;
+        result,
+        returns;
         
-    firstNumber = document.querySelector('#first-number').value;
-    secondNumber = document.querySelector('#second-number').value;
-    result = document.querySelector('#result');
+    firstNumber = document.querySelector('#subtract-first-number').value;
+    secondNumber = document.querySelector('#subtract-second-number').value;
+    result = document.querySelector('#subtract-result');
     
-    sum = +firstNumber + +secondNumber;
-    result.innerHTML = sum;
+    returns = +firstNumber - +secondNumber;
+    result.innerHTML = "= " + returns;
   }
 };
